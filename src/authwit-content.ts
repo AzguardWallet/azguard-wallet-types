02cea58ee94de6b7ef0473e6469c84ade68f79e7
Maksim Strebkov <257byte@gmail.com>
2025-02-18 23:29:48 +0300

    Add `encoded_call` authwit content

--- src/authwit-content.ts
+++ src/authwit-content.ts
@@ -1,6 +1,7 @@
 /** Content to add authwitness for. */
 export type AuthwitContent =
     | CallAuthwitContent
+    | EncodedCallAuthwitContent
     | IntentAuthwitContent
     | MessageHashAuthwitContent;
 
@@ -16,6 +17,28 @@ export type CallAuthwitContent = {
     method: string;
     /** Arguments (unencoded) */
     args: any[];
+};
+
+/** Encoded contract call to be authorized */
+export type EncodedCallAuthwitContent = {
+    /** Authwit content kind */
+    kind: "encoded_call";
+    /** Address of the caller (AztecAddress) */
+    caller: string;
+    /** Name of the function */
+    name: string;
+    /** Address of the contract (AztecAddress) */
+    to: string;
+    /** Selector of the function (FunctionSelector) */
+    selector: string;
+    /** Type of the function (FunctionType) */
+    type: string;
+    /** Whether this call can makes modifications to state or not */
+    isStatic: boolean;
+    /** Encoded arguments (Fr[]) */
+    args: string[];
+    /** Return types for decoding (AbiType[]) */
+    returnTypes: unknown[];
 };
 
 /** Arbitrary intent to be authorized */
